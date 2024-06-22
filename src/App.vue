935776d6ef1812cbf5de559d3440531bd54a50c4
Mateusz Marczak <marczak812@gmail.com>
2024-06-23 00:15:28 +0200

    Add styles to components and API books data fetch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="app">
-    <header>
+  <div id="app" class="app">
+    <header class="app__heading">
       <h1>Books<span>.app</span></h1>
     </header>
     <books-list
@@ -19,19 +19,11 @@ import BooksList from './components/BooksList'
 import BooksLengthMsg from './components/BooksLengthMsg'
 import BookForm from './components/BookForm'
 import BooksSummary from './components/BooksSummary'
+import axios from 'axios'
 export default {
   name: 'App',
   data: () => ({
-    books: [
-      {
-        title: 'The Catcher in the Rye',
-        price: 20
-      },
-      {
-        title: 'Of Mice and Men',
-        price: 18
-      }
-    ]
+    books: []
   }),
   methods: {
     removeBook (index) {
@@ -39,10 +31,40 @@ export default {
     },
     addBook (book) {
       this.books.push({ ...book })
+    },
+    async fetchBooks () {
+      try {
+        const response = await axios.get('https://api.itbook.store/1.0/new')
+        this.books = response.data.books.slice(0, 3).map(book => ({
+          title: book.title,
+          price: parseFloat(book.price.replace('$', ''))
+        }))
+      } catch (error) {
+        console.error('Error fetching books:', error)
+      }
     }
+  },
+  async created () {
+    await this.fetchBooks()
   },
   components: {
     BooksList, BooksLengthMsg, BookForm, BooksSummary
   }
 }
 </script>
+<style lang="scss">
+.app {
+  width: 100%;
+  max-width: 1000px;
+  padding: 2rem;
+  margin: 0 auto;
+
+  &__heading {
+    font-size: 3rem;
+    text-align: center;
+    span {
+      color: #5a58da;
+    }
+  }
+}
+</style>
--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
-    <ul>
+    <ul class="books-list">
       <li
+        class="books-list__item"
+        :class="{'books-list__item--promotion': book.price < 20}"
         :key="index"
         v-for="(book, index) in books">
         {{ book.title }}, {{ book.price }}
-        <button @click="$emit('remove', index)">Remove</button>
+        <button class="books-list__btn" @click="$emit('remove', index)">Remove</button>
       </li>
     </ul>
   </div>
@@ -21,3 +23,46 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.books-list {
+  font-size: 1rem;
+  list-style-type: none;
+  margin: 1rem 0;
+  padding: 1rem;
+  color: #637892;
+  font-weight: 700;
+
+  &__item {
+    padding: 1rem 0;
+    border-bottom: .0625rem dashed rgba(0,0,0,.1);
+  }
+
+  &__btn {
+    color: #fff;
+    box-shadow: 0 10px 40px 0 rgba(76,104,239,.35), 0 0 20px 0 rgba(76,104,239,.05);
+    border-radius: .3rem;
+    border: none;
+    font-family: inherit;
+    text-transform: uppercase;
+    font-weight: 700;
+    min-width: 6rem;
+    text-align: center;
+    cursor: pointer;
+    transition: .2s;
+    background: #ea2027;
+    color: #fff;
+    padding: .5rem 1rem;
+    font-size: .6rem;
+    margin: 0 .5rem;
+
+    &:hover {
+      box-shadow: 0 20px 40px 0 rgba(76,104,239,.35);
+    }
+  }
+
+  &__item--promotion {
+    font-size: 1.5rem;
+    text-decoration: underline;
+  }
+}
+</style>
